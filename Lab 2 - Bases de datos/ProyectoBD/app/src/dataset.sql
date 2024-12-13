f05de49ec45601d6f6addbaf9f667924de8dbd69
Pauk <56691890+pauca881@users.noreply.github.com>
2024-12-13 17:44:50 +0100

    Update PHP

---
+++ Lab 2 - Bases de datos/ProyectoBD/app/src/dataset.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    email VARCHAR(100)
+);
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    email VARCHAR(100)
+);
